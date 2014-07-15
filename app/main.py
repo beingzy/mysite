31f7345480bf9bbdb4a28503dde8f3232d68eea9
Yi Zhang <beingzy@gmail.com>
2014-07-14 23:04:30 -0400

    Build the structure of website
    A tentative structure of website had been developed: home, blogs,
    about_me, error(404)page. A template based on bootstrap 3.0 had been
    employed to control the appearing.

--- app/main.py
+++ app/main.py
@@ -6,32 +6,50 @@
 ## Email:beingzy@gmail.com                              ##
 ## Date: Jul/7/2014 (create date)                       ##  
 ## #################################################### ##
-from flask import Flask
-from flask import url_for
-from flask import render_template
-from flask import request
-from flask import jsonify
-## bootstrap
-from flask.ext.bootstrap import Bootstrap
-# app = Flask(__name__)
-bootstrap = Bootstrap(__name__)
+import os
+from flask import Flask, url_for, render_template, request, g, jsonify, abort, redirect, url_for, escape, session
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Load default config and overwrite config from an environment variable
+app.config.update(dict(
+	DEBUG=True,
+	SECRET_KEY='\xe0|,Q\xbf\x9a\xd7;\x0c\x96\x84b<\xc3\x94\x00:\x0e)]C',
+	USERNAME='admin',
+	PASSWORD='default'
+	))
       
 ## #############
 ## Website 
-@app.route('/')
+@app.route('/', methods=['GET', "POST"])
 def index():
-	return render_template('index.html', foo=42)
+	# username = request.cookies.get('username')
+	static_url = url_for('static', filename='images/favicon.ico')
+	pwd_path   = os.getcwd()
+	response = render_template('index.html', static_url=pwd_path)
+	# response.set_cookie('username', 'the_username')
+	return response
 
 ## Blogs
-@app.route('/blogs')
+@app.route('/blogs/')
 def blogs():
 	return render_template('blogs.html', foo=42)
 
 ## Resume
-@app.route('/aboutme')
-def resume():
-	return render_template('aboutme.html', foo=42)
+@app.route('/aboutme/')
+def aboutme():
+	#return render_template('aboutme.html', foo=42)
+	return redirect(url_for('error/'))
+
+@app.route('/error/')
+@app.errorhandler(404)
+def error_page():
+	resp = make_response(render_template('error.html'), 404)
+	resp.headers['X-Somthing'] = "Error: 404"
+	return resp
 
 if __name__ == '__main__':
-	## to envure when the file is excuted directly
-	app.run(debug=True)
+	app.run(debug=True, host='0.0.0.0', port=8000)
+
+
