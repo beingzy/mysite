6c0775a788ce950f75bb4a993c87ad0c7aa2e989
Yi Zhang <beingzy@gmail.com>
2014-07-26 12:18:41 -0400

    Modularize the entire app
    Add config.py, models.py and setup.py

--- app/main.py
+++ app/main.py
@@ -11,44 +11,20 @@ import datetime
 from flask import Flask, url_for, render_template, request, g, jsonify, abort, redirect, url_for, escape, session
 from flask.ext.mongoengine import MongoEngine
 from mongoengine import *
-# import models
+# Import configuration (Config/ProdConfig/DevConfig/TestConfig)
+import config
+# Import MongoDB Models/Schema
+from models import *
 
 app = Flask(__name__)
-#app.config.from_pyfile('the-config.cfg')
-app.config.update(dict(
-	DEBUG=True,
-	SECRET_KEY='\xe0|,Q\xbf\x9a\xd7;\x0c\x96\x84b<\xc3\x94\x00:\x0e)]C',
-	USERNAME='admin',
-	PASSWORD='default'
-	))
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+app.config.from_object('config.DevConfig')
 
-# MongoDB Connection
-connect('beingzy_site', host='54.88.134.182', port=27017)
-# db = MongoEngine(__name__)
-
-class Blog(Document):
-	title   = StringField(max_length=256, required=True)
-	created = DateTimeField(default=datetime.datetime.now,   required=True)
-	updated = DateTimeField(default=datetime.datetime.now,   required=True)
-	author  = StringField(max_length=128, default="beingzy", required=True)
-	body    = StringField(required=True)
-	tags    = ListField(StringField(), required=False)
-
-class Resume(Document):
-	first_name  = StringField(max_length=128, required=True)
-	last_name   = StringField(max_length=128, required=True)
-	middle_name = StringField(max_length=128, required=False)
-	phone       = StringField(max_length=128, required=True)
-	email       = EmailField(required=True)
-	version     = FloatField(required=True)
-
-class Moto(Document):
-	created = DateTimeField(default=datetime.datetime.now,   required=True)
-	body    = StringField(max_length=1000, required=True)
-	tags    = ListField(StringField(), required=True) 
+connect(app.config['MONGODB_SETTINGS']['DB'], 
+	host=app.config['MONGODB_SETTINGS']['HOST'], 
+	port=app.config['MONGODB_SETTINGS']['PORT'])
 
 def get_blog_from_db():
+	# Read blogs and sort by date from the most recent one
 	blogs = Blog.objects.order_by("-created")
 	return blogs
 
@@ -63,9 +39,7 @@ def get_resume(version=None):
 ## Website 
 @app.route('/', methods=['GET', "POST"])
 def index():
-	# username = request.cookies.get('username')
 	return render_template('index.html')
-	# response.set_cookie('username', 'the_username')
 
 ## Blogs
 @app.route('/blogs/')
@@ -76,7 +50,6 @@ def blogs():
 ## About Me
 @app.route('/aboutme/')
 def aboutme():
-	#return render_template('aboutme.html', foo=42)
 	resume = get_resume()
 	return render_template('aboutme.html', resume=resume)
 
