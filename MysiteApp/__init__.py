e9af716b1ba8ac2eff7f9c1420f641d0cc99721a
Yi Zhang <beingzy@gmail.com>
2014-08-03 09:14:38 -0400

    delete venv/* and *.pyc

---
+++ MysiteApp/__init__.py
@@ -0,0 +1,69 @@
+## ##################################################### ##
+## Website structure:                                    ##
+##   homepage(index.html) / blogs (blog.html) / aboutme  ##
+##                                                       ##  
+## Author: Yi Zhang                                      ##  
+## Email: beingzy@gmail.com                              ##
+## Date: Jul/14/2014 (update date)                       ##  
+## ##################################################### ##
+import os
+import datetime
+from flask import Flask, url_for, render_template, request, g, jsonify, abort, redirect, url_for, escape, session
+#from flask.ext.mongoengine import MongoEngine
+from mongoengine import *
+# Import configuration (Config/ProdConfig/DevConfig/TestConfig)
+import config
+# Import MongoDB Models/Schema
+from models import *
+
+app = Flask(__name__)
+app.config.from_object('config.DevConfig')
+
+connect(app.config['MONGODB_SETTINGS']['DB'], 
+	host=app.config['MONGODB_SETTINGS']['HOST'], 
+	port=app.config['MONGODB_SETTINGS']['PORT'])
+
+def get_blog_from_db():
+	# Read blogs and sort by date from the most recent one
+	blogs = Blog.objects.order_by("-created")
+	return blogs
+
+def get_resume(version=None):
+	if version is None:
+		resumes = Resume.objects
+		resume  = resumes[len(resumes) - 1]
+	else:
+		resume  = Resume.objects(version=version)
+	return resume
+
+## Website 
+@app.route('/', methods=['GET', "POST"])
+def index():
+	return render_template('index.html')
+
+## Blogs
+@app.route('/blogs/')
+def blogs():
+	blogs = get_blog_from_db()
+	return render_template('blogs.html', blogs=blogs)
+
+## About Me
+@app.route('/aboutme/')
+def aboutme():
+	resume = get_resume()
+	return render_template('aboutme.html', resume=resume)
+
+@app.errorhandler(404)
+def error_page():
+	return render_template('error.html'), 404
+
+@app.route('/user/<username>')
+def user(username):
+	return "username is %s" % username
+
+
+if __name__ == '__main__':
+	#app.run(debug=True, host='0.0.0.0', port=8000)
+	app.run(debug=app.config['DEBUG'], host=app.config['HOST'], port=app.config['PORT'])
+
+
---
+++ MysiteApp/config.py
@@ -0,0 +1,29 @@
+class Config(object):
+    DEBUG   = False
+    TESTING = False
+    HOST    = '0.0.0.0'
+    PORT    = 8000
+    MONGODB_SETTINGS = {
+    'DB': "beingzy_site", 
+    'HOST': '0.0.0.0',
+    'PORT': 27017
+    }
+
+class ProdConfig(Config):
+	pass
+
+class DevConfig(Config):
+	DEBUG = True
+	MONGODB_SETTINGS = {
+    'DB': "beingzy_site", 
+    'HOST': '54.88.134.182',
+    'PORT': 27017
+    }
+
+class TestConfig(Config):
+	TESTING = True
+	MONGODB_SETTINGS = {
+    'DB': "beingzy_site", 
+    'HOST': '54.88.134.182',
+    'PORT': 27017
+    }
